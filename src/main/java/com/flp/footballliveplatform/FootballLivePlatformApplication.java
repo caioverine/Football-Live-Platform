2b0e6f7e53fb249cc6315acd5d9851ea45d41ab9
DESKTOP-8A4HGFL\caioverine <viniciuscaio.17@gmail.com>
2026-08-17 18:27:18 -0300

    initial commit

---
+++ src/main/java/com/flp/footballliveplatform/FootballLivePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.flp.football_live_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballLivePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballLivePlatformApplication.class, args);
+	}
+
+}
